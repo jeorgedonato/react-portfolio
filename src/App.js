1ca545c043bb93ac1af81823a979e3db2a3d3bc9
Jeorge Donato <jeorgekhenr@gmail.com>
2020-11-03 20:58:12 -0800

    Modified app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import { Helmet } from 'react-helmet';
+import WebIcon from './components/Images/webicon.png';
+import './App.css';
 
 function App() {
   return (
-   <>
+   <div>
+   <Helmet>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta property="og:title" content="Jeorge Donato" />
+      <meta name="author" content="Jeorge Donato" />
+      <meta property="og:locale" content="en_US" />
+      <meta name="description" content="Full Stack Web Developer" />
+      <meta property="og:description" content="Full Stack Web Developer" />
+      <title>Jeorge Donato | Full Stack Web Developer</title>
+      <link rel="icon" href={WebIcon}></link>
+   </Helmet>
    <Navbar />
-   </>
+   </div>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,45 @@
-import React from 'react'
-
+import React,{useRef, useState} from 'react';
+import './style.css';
+import Banner from '../Images/portfolio-banner.jpg';
+  
 const Navbar = () => {
+  const nav = useRef(null);
+  const [navClass,setNavClass] = useState("");
+  let navClassnames = ["nav-links"];
+
+  const navSlide = () => {
+    setNavClass(navClass ? "" : "nav-active");
+    navClassnames.push(navClass);
+    console.log(navClassnames.join(" "))
+  };
   return (
     <>
-    <nav>
-
-    </nav>
+      <nav >
+        <div className="logo">
+          <h4>The Nav</h4>
+          </div>
+          <ul className={navClassnames.join(" ")} ref={nav}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Work</a>
+            </li>
+            <li>
+              <a href="/">Projects</a>
+            </li>
+          </ul>
+          <div className="burger" onClick={()=> navSlide()}>
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+      </nav>
     </>
-  )
+  );
 };
 
 export default Navbar;
