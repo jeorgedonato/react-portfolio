76afeab5201dd2ab3808a15dc0cf669cdd15ffa6
Jeorge Donato <jeorgekhenr@gmail.com>
2020-11-04 08:35:30 -0800

    Added Navbar animations

---
+++ src/components/Listlinks/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Listlinks = props => {
+
+  return (
+    <>
+      <li>
+        <a href="/">{props.title}</a>
+      </li>
+    </>
+  )
+
+};
+
+export default Listlinks
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,36 +1,39 @@
 import React,{useRef, useState} from 'react';
 import './style.css';
 import Banner from '../Images/portfolio-banner.jpg';
+import Listlinks from '../Listlinks';
   
 const Navbar = () => {
-  const nav = useRef(null);
-  const [navClass,setNavClass] = useState("");
-  let navClassnames = ["nav-links"];
+  const ulNav = useRef(null);
+  let initialNavClass = ["nav-links"];
+  const [navClass,setNavClass] = useState("nav-active");
+  const [navClassNames,setNavClassNames] = useState(initialNavClass);
+  
 
   const navSlide = () => {
-    setNavClass(navClass ? "" : "nav-active");
-    navClassnames.push(navClass);
-    console.log(navClassnames.join(" "))
+    setNavClass(!navClass ? "nav-active" : "");
+    setNavClassNames([...initialNavClass, navClass]);
+    // console.log(navClassNames.join(" "));
+    // const navLinks = document.querySelectorAll('.nav-links li');
+    const navLinks = ulNav.current.children;
+    let navI = 0;
+    for (let nl of navLinks) {
+        nl.style.animation = `navLinkFade 0.5s ease forwards ${navI /2.5}s`;
+        navI++;
+    }
+   
   };
   return (
     <>
       <nav >
         <div className="logo">
-          <h4>The Nav</h4>
+          <h4>Jeorge Donato</h4>
           </div>
-          <ul className={navClassnames.join(" ")} ref={nav}>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">About</a>
-            </li>
-            <li>
-              <a href="/">Work</a>
-            </li>
-            <li>
-              <a href="/">Projects</a>
-            </li>
+          <ul className={navClassNames.join(" ")} ref={ulNav}>
+            <Listlinks title="Home" />
+            <Listlinks title="About" />
+            <Listlinks title="Projects" />
+            <Listlinks title="About" />
           </ul>
           <div className="burger" onClick={()=> navSlide()}>
             <div className="line1"></div>
