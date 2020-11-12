e20f15ec5f795dd4c05b979dd5bcb76d16588c32
Jeorge Donato <jeorgekhenr@gmail.com>
2020-11-11 18:21:37 -0800

    Added react lazy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar';
+import React,{Suspense,lazy} from 'react'
+// import Navbar from './components/Navbar';
 import { Helmet } from 'react-helmet';
 import WebIcon from './components/Images/webicon.png';
-import Landing from './components/Landing';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Footer from './components/Footer';
+// import Landing from './components/Landing';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import Skills from './components/Skills';
+// import Footer from './components/Footer';
 import './App.css';
+
+const About  = lazy(() => import('./components/About'));
+const Landing  = lazy(() => import('./components/Landing'));
+const Projects  = lazy(() => import('./components/Projects'));
+const Skills  = lazy(() => import('./components/Skills'));
+const Navbar  = lazy(() => import('./components/Navbar'));
+const Footer  = lazy(() => import('./components/Footer'));
 
 function App() {
   return (
@@ -24,12 +31,15 @@ function App() {
         <link rel="icon" href={WebIcon}></link>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
     </Helmet>
-    <Navbar />
-    <Landing />
-    <About />
-    <Projects />
-    <Skills />
-    <Footer />
+    <Suspense fallback={<h3>Loading…</h3>}>
+      <Navbar />
+      <Landing />
+      <About />
+      <Projects />
+      <Skills />
+      <Footer />
+    </Suspense>
+    
    </div>
   );
 }
